b40516a9706fa6f3ee75516ac89355a1e943c7a2
Belkis Ceri <belkisceri@gmailcom>
2021-11-05 11:02:42 -0700

    Solutions

---
+++ Anwers_for_practice_problems_week1.py
@@ -0,0 +1,88 @@
+
+#p_1.
+def addition(x,y):   # problem 1
+    sum = x + y
+    return sum
+
+
+print(addition(-2,-5))
+
+##################
+#p_2.
+def addition(parameter):   # problem 2
+    return parameter + 1
+
+
+print("addition " +str(addition(-3)))
+##################
+#p_3.
+
+def convert(n):   # problem 3
+    second = (n*60)
+    return second
+
+
+print(str(convert(10)) + " seconds")
+
+##################
+#p_4.
+
+h = int()
+b = int()
+
+
+def tri_area( h, b):       # problem 4
+    area = ((h * b)/2)
+    return area
+
+
+print("area of the triangle = " , (tri_area(10,20)))
+##################
+#p_5.                            # problem 5
+def next_edge(a, b):
+    c =(a+b)-1
+    return c
+
+
+print("max_length is " + str(next_edge(8, 10)))
+##################
+#p_6.
+
+def stutter (my_word):       # problem 6
+    my_ellipsis = "... "
+    question_mark = "?"
+    first_two = my_word [:2]
+    my_pattern = first_two + my_ellipsis
+    result = my_pattern + my_pattern + my_word + question_mark
+
+    return result
+
+
+print(stutter("incredible"))
+##################
+#p_7.
+def dis(org_price, percent):                # problem 7
+    final_price = int(org_price-(org_price*percent/100))
+    return final_price
+
+print(int(dis(1500, 50)))
+##################
+#p_8.
+def relation_to_luke(relation):             # problem 8
+    if relation == "Darth Vader":
+        print("Luke, I am your father.")
+    else:
+        if relation == "Leia":
+            print("Luke, I am your sister.")
+        else:
+            if relation == "Han":
+                print("Luke, I am your brother in law.")
+            else:
+                if relation == "R2D2":
+                    print("Luke, I am a droid.")
+
+relation_to_luke("R2D2")
+
+
+
+
