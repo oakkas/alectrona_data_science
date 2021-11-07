72ee70b9ac38bcbaf410b80a8399ce782a05b6c7
sarslana <93412945+sarslana@users.noreply.github.com>
2021-11-07 13:58:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-x = [1, 4, 6, 9, 10]
-y = []
-for i in x:
-  y.append(i*i)
-print(y)
+#p_1. Create a function that takes two numbers as arguments and return their sum.
+
+#Examples
+#addition(3, 2) ➞ 5
+#addition(-3, -6) ➞ - 9
+#addition(7, 3) ➞ 10
+print("Welcome to addition calculator")
+first_number=float(input('What is your first number? '))
+second_number=float(input('What is your second number? '))
+sum= float(first_number+second_number)
+print(f'Addition of your number is {sum}')
+print('Thank you for using Addition Calculator')
